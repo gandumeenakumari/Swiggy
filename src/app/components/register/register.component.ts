d055ecb638ed287f03a9ec7ce5dc194e99a4d765
meenakumarigandu <meenakumarigandu@gmail.com>
2022-10-27 20:16:57 +0530

    swiggy

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -8,30 +8,39 @@ import { last } from 'rxjs';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+repeatpass:string='name';
   constructor() { }
 
   ngOnInit(): void {
   }
 registerForm=new FormGroup({
 firstname:new FormControl("",[Validators.required,Validators.minLength(2),Validators.pattern("[a-zA-Z].*")]),
-lastname:new FormControl(""),
-email:new FormControl(""),
-mobile:new FormControl(""),
-gender:new FormControl(""),
-pwd:new FormControl(""),
-rpwd:new FormControl("")
+lastname:new FormControl("",[Validators.required,Validators.minLength(2),Validators.pattern("[a-zA-Z].*")]),
+email:new FormControl("",[Validators.required,Validators.email]),
+mobile:new FormControl("",[Validators.required,Validators.pattern("[0-9]*"),Validators.minLength(10),Validators.maxLength(10),]),
+gender:new FormControl("",[Validators.required]),
+pwd:new FormControl("",[Validators.required,Validators.minLength(6),Validators.maxLength(15),]),
+rpwd:new FormControl(""),
+
 
 }
 );
 
 registerSubmitted(){
+  if(this.Pwd.value==this.Rpwd.value)
+  {
+    console.log(this.registerForm.valid);
+    this.repeatpass='none'
+  }
+  else{
+    this.repeatpass='inline'
+  }
   console.log(this.registerForm.get("firstname"));
 }
 get FirstName():FormControl{
   return this.registerForm.get("firstname") as FormControl;
 }
-get LatName():FormControl{
+get LastName():FormControl{
   return this.registerForm.get("lastname") as FormControl;
 }
 get Email():FormControl{
@@ -45,5 +54,8 @@ get Gender():FormControl{
 }
 get Pwd():FormControl{
   return this.registerForm.get("pwd") as FormControl;
+}
+get Rpwd():FormControl{
+  return this.registerForm.get("rpwd") as FormControl;
 }
 }
